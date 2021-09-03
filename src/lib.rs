9950421159c9da890a1ecb1a340ae5a0c77259d4
Thomas Locher <thomas.locher@dfinity.org>
2021-09-03 11:23:53 +0200

    The lib module will hold all the library functions.

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+mod secret_sharing;
+
+/// The function is called to create secret-shared passphrases.
+pub fn create() {
+    println!("TODO: This function will be invoked when creating shares.");
+}
+
+/// The function is called to reconstruct a passphrase.
+pub fn reconstruct() {
+    println!("TODO: This function will be invoked when reconstructing a passphrase.");
+}
