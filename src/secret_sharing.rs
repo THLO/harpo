601b948e9becadcc157183d97b13fc57c95e7dd1
Thomas Locher <thomas.locher@dfinity.org>
2021-09-13 13:06:06 +0200

    Defined prime numbers for all supported key sizes.

--- src/secret_sharing.rs
+++ src/secret_sharing.rs
@@ -3,14 +3,33 @@ use num_traits::One;
 use crate::math::{FiniteFieldElement, get_random_number};
 use std::fmt;
 
+/// For each modulus, the largest prime number is used for each bit length.
+/// These prime numbers can be found here: https://primes.utm.edu/lists/2small/200bit.html
+
+/// The prime number
+/// 2^128-159 = 340282366920938463463374607431768211297
+/// is used for 128-bit inputs.
+pub const MODULUS_ARRAY_128 : [u32; 4] = [u32::MAX-158, u32::MAX, u32::MAX, u32::MAX];
+
+/// The prime number
+/// 2^160-47 = 1461501637330902918203684832716283019655932542929
+/// is used for 160-bit inputs.
+pub const MODULUS_ARRAY_160 : [u32; 5] = [u32::MAX-46, u32::MAX, u32::MAX, u32::MAX, u32::MAX];
+
+/// The prime number
+/// 2^192-237 = 6277101735386680763835789423207666416102355444464034512659
+/// is used for 192-bit inputs.
+pub const MODULUS_ARRAY_192 : [u32; 6] = [u32::MAX-236, u32::MAX,u32::MAX, u32::MAX, u32::MAX, u32::MAX];
+
+/// The prime number
+/// 2^224-63 = 26959946667150639794667015087019630673637144422540572481103610249153
+/// is used for 224-bit inputs.
+pub const MODULUS_ARRAY_224 : [u32; 7] = [u32::MAX-62, u32::MAX, u32::MAX, u32::MAX, u32::MAX, u32::MAX, u32::MAX];
+
 /// The prime number
 /// 2^256-189 = 115792089237316195423570985008687907853269984665640564039457584007913129639747
-/// is used. It is the largest prime number below 2^256
-/// (see https://primes.utm.edu/lists/2small/200bit.html), which means that all 256-bit numbers
-/// except the 189 largest can be represented.
-pub const MODULUS_ARRAY : [u32; 8] = [u32::MAX-188, u32::MAX,u32::MAX, u32::MAX,u32::MAX, u32::MAX, u32::MAX,u32::MAX];
-
-pub const MODULUS_SIZE_BITS : u32 = 256;
+/// is used for 256-bit inputs.
+pub const MODULUS_ARRAY_256 : [u32; 8] = [u32::MAX-188, u32::MAX, u32::MAX, u32::MAX, u32::MAX, u32::MAX, u32::MAX, u32::MAX];
 
 struct Polynomial {
     coefficients: Vec<BigUint>,
@@ -30,10 +49,10 @@ impl fmt::Display for SecretShare {
 
 impl Polynomial {
 
-    fn new(degree: u32, modulus: &BigUint) -> Self {
+    fn new(degree: u32, num_bits: u32, modulus: &BigUint) -> Self {
         let mut coefficients = vec![];
         for _in in 0..=degree {
-            coefficients.push(get_random_number(MODULUS_SIZE_BITS, modulus));
+            coefficients.push(get_random_number(num_bits, modulus));
         }
         Polynomial {
             coefficients, modulus: modulus.clone()
@@ -56,12 +75,13 @@ impl Polynomial {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use num_traits::Zero;
 
     #[test]
     /// The function tests the evaluation of a polynomial:
     fn test_polynomial_evaluation() {
         let modulus = BigUint::from_slice(&[127]);
-        let polynomial = Polynomial::new(2, &modulus);
+        let polynomial = Polynomial::new(2, 7, &modulus);
         // Evaluate the polynomial at 0:
         assert_eq!(polynomial.evaluate(&Zero::zero()), polynomial.coefficients[0]);
         // Evaluate the polynomial at 1 (which should be the sum of coefficients):
