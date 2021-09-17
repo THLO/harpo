6fb7224b91578ea7208caa4939e3a49ad897107c
Thomas Locher <thomas.locher@dfinity.org>
2021-09-17 15:04:07 +0200

    Added the word_list module.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ mod math;
 /// The secret_sharing module provides the secret-sharing functionality.
 mod secret_sharing;
 
+/// The default word list is loaded from a separate module.
+mod word_list;
+
 /// The function is called to create secret-shared passphrases.
 pub fn create() {
     println!("TODO: This function will be invoked when creating shares.");
